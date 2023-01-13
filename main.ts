c334891dfdc913717cb1e2d9d08e1952191e96ae
shreya-40005 <122539803+shreya-40005@users.noreply.github.com>
2023-01-13 08:56:49 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,6 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
 })
 basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    music.playMelody("C5 A E G C D F B ", 170)
+})
